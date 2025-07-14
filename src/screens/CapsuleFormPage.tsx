de87f904950c95f426dbace333e5448fa5ee5b02
dud1111122 <yeung0708@naver.com>
2025-07-15 07:35:33 +0900

    feat: CapsuleFormPage 수정

--- src/screens/CapsuleFormPage.tsx
+++ src/screens/CapsuleFormPage.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { Modal, Pressable, View, Text, TextInput, Button, Image, Platform } from 'react-native';
+import { Modal, Pressable, View, Text, TextInput, Button, Image, Platform , TouchableWithoutFeedback, Keyboard} from 'react-native';
 import { CaretLeft } from 'phosphor-react-native';
 import CustomButton from '../components/UI/CustomButton';
 import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 export default function CapsuleFormPage() {
   const [CapsuleOpenVisible, setCapsuleOpenVisible] = useState(false);
@@ -71,151 +72,144 @@ export default function CapsuleFormPage() {
           <TextInput
             multiline
             value={CapsuleDescribe}
-            onChangeText={(text) => {
-              if (text.endsWith('\n')) {
-                const trimmed = text.replace(/\n$/, '');
-                setCapsuleDescribe(trimmed);
-                setCapsuleOpenVisible(true);
-              } else {
-                setCapsuleDescribe(text);
-              }
-            }}
+            onChangeText={(text) => {setCapsuleDescribe(text);}}
             placeholder="이곳에 캡슐에 보관할 글을 작성해주세요."
             placeholderTextColor="gray"
             textAlign="left"
             textAlignVertical="top"
             className="text-[16px]"
           />
-
+          
           <Modal
             visible={CapsuleOpenVisible}
             transparent
             animationType="slide"
             onRequestClose={() => setCapsuleOpenVisible(false)}
           >
-            <View className="flex-1 items-center justify-end">
-              <View
-                style={{ height: '60%' }}
-                className="w-screen rounded-2xl bg-white p-6 shadow-md border border-gray-400"
-              >
-                <Text className="text-lg font-bold mb-2">캡슐 등록</Text>
+            <TouchableWithoutFeedback onPress={() => setCapsuleOpenVisible(false)}>
+              <View className="flex-1 justify-end bg-transparent">
+                <TouchableWithoutFeedback onPress={() => null}>
+                  <View className="w-screen rounded-2xl bg-white p-6 border  border-gray-400" style={{ height: '60%' }}>
+                    <Text className="text-lg font-bold mb-2">캡슐 등록</Text>
 
-                <View className='flex flex-col space-y-[42px]'>
-                  <Text className='text-[24px]'>{CapsuleTitle}</Text>
+                    <View className='flex flex-col space-y-[42px]'>
+                      <Text className='text-[24px]'>{CapsuleTitle}</Text>
 
-                  <View className='flex-row items-center'>
-                    <Text className='w-[80px] text-black font-bold'>캡슐 종류</Text>
-                    <View className='flex-row space-x-[20px]'>
-                      <Pressable
-                        className="flex-row items-center space-x-1"
-                        onPress={() => setSelectedType('normal')}
-                      >
-                        <View
-                          className={`w-4 h-4 rounded-full border-2 ${
-                            selectedType === 'normal' ? 'border-purple3' : 'border-gray-400'
-                          } items-center justify-center`}
-                        >
-                          {selectedType === 'normal' && (
-                            <View className="w-2 h-2 rounded-full bg-purple3" />
-                          )}
+                      <View className='flex-row items-center'>
+                        <Text className='w-[80px] text-black font-bold'>캡슐 종류</Text>
+                        <View className='flex-row space-x-[20px]'>
+                          <Pressable
+                            className="flex-row items-center space-x-1"
+                            onPress={() => setSelectedType('normal')}
+                          >
+                            <View
+                              className={`w-4 h-4 rounded-full border-2 ${
+                                selectedType === 'normal' ? 'border-purple3' : 'border-gray-400'
+                              } items-center justify-center`}
+                            >
+                              {selectedType === 'normal' && (
+                                <View className="w-2 h-2 rounded-full bg-purple3" />
+                              )}
+                            </View>
+                            <Text
+                              className={`$${
+                                selectedType === 'normal' ? 'text-purple3 font-semibold' : 'text-gray-400'
+                              }`}
+                            >
+                              일반 캡슐
+                            </Text>
+                          </Pressable>
+
+                          <Pressable
+                            className="flex-row items-center space-x-1"
+                            onPress={() => setSelectedType('time')}
+                          >
+                            <View
+                              className={`w-4 h-4 rounded-full border-2 ${
+                                selectedType === 'time' ? 'border-purple3' : 'border-gray-400'
+                              } items-center justify-center`}
+                            >
+                              {selectedType === 'time' && (
+                                <View className="w-2 h-2 rounded-full bg-purple3" />
+                              )}
+                            </View>
+                            <Text
+                              className={`$${
+                                selectedType === 'time' ? 'text-purple3 font-semibold' : 'text-gray-400'
+                              }`}
+                            >
+                              타임 캡슐
+                            </Text>
+                          </Pressable>
                         </View>
-                        <Text
-                          className={`$${
-                            selectedType === 'normal' ? 'text-purple3 font-semibold' : 'text-gray-400'
-                          }`}
-                        >
-                          일반 캡슐
-                        </Text>
-                      </Pressable>
+                      </View>
 
-                      <Pressable
-                        className="flex-row items-center space-x-1"
-                        onPress={() => setSelectedType('time')}
-                      >
-                        <View
-                          className={`w-4 h-4 rounded-full border-2 ${
-                            selectedType === 'time' ? 'border-purple3' : 'border-gray-400'
-                          } items-center justify-center`}
+                      <View className='flex-row items-center'>
+                        <Text className='w-[80px] text-black font-bold'>위치</Text>
+                        <Text className='text-gray-400'>지도에서 찾기</Text>
+                      </View>
+
+                      <View className='flex-row items-center space-x-4'>
+                        <Text className='w-[65px] text-black font-bold'>오픈일</Text>
+                        <Pressable onPress={() => setShowPicker(true)}>
+                          <Text className="text-gray-600">
+                            {OpenAtDate.toLocaleDateString('ko-KR')}
+                          </Text>
+                        </Pressable>
+                        <Pressable onPress={() => setShowTimePicker(true)}>
+                          <Text className="text-gray-600">
+                            {OpenAtTime.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false })}
+                          </Text>
+                        </Pressable>
+                      </View>
+
+                      {showPicker && (
+                        <DateTimePicker
+                          value={OpenAtDate}
+                          mode="date"
+                          display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                          onChange={(event, selectedDate) => {
+                            if (Platform.OS === 'android') setShowPicker(false);
+                            if (selectedDate) setOpenAtDate(selectedDate);
+                          }}
+                        />
+                      )}
+
+                      {showTimePicker && (
+                        <DateTimePicker
+                          value={OpenAtTime}
+                          mode="time"
+                          display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                          is24Hour={true}
+                          onChange={(event, selectedTime) => {
+                            if (Platform.OS === 'android') setShowTimePicker(false);
+                            if (selectedTime) setOpenAtTime(selectedTime);
+                          }}
+                        />
+                      )}
+
+                      <View className='flex-row justify-between px-6 w-full'>
+                        <CustomButton
+                          className="w-[120px] bg-white p-2 border border-gray-300 rounded-full"
+                          textClassName="font-bold color-black text-[10px]"
+                          onPress={() => setCapsuleOpenVisible(false)}
                         >
-                          {selectedType === 'time' && (
-                            <View className="w-2 h-2 rounded-full bg-purple3" />
-                          )}
-                        </View>
-                        <Text
-                          className={`$${
-                            selectedType === 'time' ? 'text-purple3 font-semibold' : 'text-gray-400'
-                          }`}
+                          취소
+                        </CustomButton>
+
+                        <CustomButton
+                          className="w-[120px] bg-purple3 p-2 rounded-full"
+                          textClassName="font-bold color-white text-[10px]"
+                          onPress={() => setCapsuleOpenVisible(false)}
                         >
-                          타임 캡슐
-                        </Text>
-                      </Pressable>
+                          업로드
+                        </CustomButton>
+                      </View>
                     </View>
                   </View>
-
-                  <View className='flex-row items-center'>
-                    <Text className='w-[80px] text-black font-bold'>위치</Text>
-                    <Text className='text-gray-400'>지도에서 찾기</Text>
-                  </View>
-
-                  <View className='flex-row items-center space-x-4'>
-                    <Text className='w-[65px] text-black font-bold'>오픈일</Text>
-                    <Pressable onPress={() => setShowPicker(true)}>
-                      <Text className="text-gray-600">
-                        {OpenAtDate.toLocaleDateString('ko-KR')}
-                      </Text>
-                    </Pressable>
-                    <Pressable onPress={() => setShowTimePicker(true)}>
-                      <Text className="text-gray-600">
-                        {OpenAtTime.toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit', hour12: false })}
-                      </Text>
-                    </Pressable>
-                  </View>
-
-                  {showPicker && (
-                    <DateTimePicker
-                      value={OpenAtDate}
-                      mode="date"
-                      display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                      onChange={(event, selectedDate) => {
-                        if (Platform.OS === 'android') setShowPicker(false);
-                        if (selectedDate) setOpenAtDate(selectedDate);
-                      }}
-                    />
-                  )}
-
-                  {showTimePicker && (
-                    <DateTimePicker
-                      value={OpenAtTime}
-                      mode="time"
-                      display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                      is24Hour={true}
-                      onChange={(event, selectedTime) => {
-                        if (Platform.OS === 'android') setShowTimePicker(false);
-                        if (selectedTime) setOpenAtTime(selectedTime);
-                      }}
-                    />
-                  )}
-
-                  <View className='flex-row justify-between px-6 w-full'>
-                    <CustomButton
-                      className="w-[120px] bg-white p-2 border border-gray-300 rounded-full"
-                      textClassName="font-bold color-black text-[10px]"
-                      onPress={() => setCapsuleOpenVisible(false)}
-                    >
-                      취소
-                    </CustomButton>
-
-                    <CustomButton
-                      className="w-[120px] bg-purple3 p-2 rounded-full"
-                      textClassName="font-bold color-white text-[10px]"
-                      onPress={() => setCapsuleOpenVisible(false)}
-                    >
-                      업로드
-                    </CustomButton>
-                  </View>
-                </View>
+                </TouchableWithoutFeedback>
               </View>
-            </View>
+            </TouchableWithoutFeedback>
           </Modal>
         </View>
       </View>
