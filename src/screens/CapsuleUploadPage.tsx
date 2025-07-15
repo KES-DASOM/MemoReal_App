a04646ab0631d7f0424f4f2258dc622bd31423e1
ChoYoungJoo <yeung0708@naver.com>
2025-07-15 18:20:40 +0900

    #16 [feat] 타임캡슐 캡슐등록, 업로드 UI 구현
    * CapsuleFormPage 구현
    
    * [feat] CapsuleFormPage 구현
    
    * [setting] DateTimePicker 라이브러리 추가
    
    * [feat]CapsuleUploadPage 구현, progress, svg 라이브러리 추가
    
    * feat: CapsuleFormPage 디자인 및 기능 업데이트
    
    * feat: CapsuleFormPage 수정

--- src/screens/CapsuleUploadPage.tsx
+++ src/screens/CapsuleUploadPage.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Image } from 'react-native';
+import * as Progress from 'react-native-progress';
 
 export default function CapsuleUploadPage() {
+  const [pct, setPct] = useState(0.2);
+  
+  useEffect(() => {
+    // 1초(1000ms)마다 실행
+    const id = setInterval(() => {
+      setPct(prev => {
+        const next = +(prev + 0.03).toFixed(3); // 0.1씩 증가 (원하면 조정)
+        return next >= 1 ? 0 : next;           // 1.0 넘으면 0으로 리셋
+      });
+    }, 100);
+
+    // 컴포넌트 언마운트 시 인터벌 정리
+    return () => clearInterval(id);
+  }, []);
+
+
 
   return (
-    <View>
-      <Text>타임캡슐-업로드</Text>
-    </View>
+    <View className='flex-1 bg-white px-4 justify-between'>
+          <View>
+            <Text className="text-2xl mb-5 text-center">업로드</Text>
+
+            {/* AR 삽입 예정 */}
+            <Image
+              source={require('../assets/images/logo.png')}
+              className="w-[78px] h-[78px] self-center"
+            />
+            
+          </View>
+          <Progress.Bar           // ▮ 막대(bar) 형태
+            progress={pct}        // 0 ~ 1 사이
+            width={null}          // 전체 폭(flex)
+            height={12}
+            borderRadius={8}
+            indeterminate={false} // true면 로딩 스피너 모드
+            animated              // 값 변할 때 부드럽게
+            className='mb-[50px]'
+            />
+
+        </View>
   );
 }
