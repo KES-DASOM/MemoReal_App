705ed76db12bb5546fed6f6b23adc668a55967d6
SeongHwan <epaktldk0203@gmail.com>
2025-07-21 10:24:03 +0900

    #27 [feat] Unity AR 연동
    * feat: unity 연동 라이브러리 추가
    
    * feat: RN - Unity 연동 설정
    
    * feat: AR(테스트버전) 프로젝트 추가
    
    * feat: 캡슐 생성 페이지 AR 추가

--- src/screens/CapsuleUploadPage.tsx
+++ src/screens/CapsuleUploadPage.tsx
@@ -1,8 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import { View, Text, Image } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {View, Text, Image} from 'react-native';
 import * as Progress from 'react-native-progress';
+import UnityView, { UnityModule } from '@azesmway/react-native-unity';
+
+interface IMessage {
+  gameObject: string;
+  methodName: string;
+  message: string;
+}
 
 export default function CapsuleUploadPage() {
+  const unityRef = useRef<UnityView>(null);
   const [pct, setPct] = useState(0.2);
 
   useEffect(() => {
@@ -10,15 +18,35 @@ export default function CapsuleUploadPage() {
     const id = setInterval(() => {
       setPct(prev => {
         const next = +(prev + 0.03).toFixed(3); // 0.1씩 증가 (원하면 조정)
-        return next >= 1 ? 0 : next;           // 1.0 넘으면 0으로 리셋
+        return next >= 1 ? 0 : next; // 1.0 넘으면 0으로 리셋
       });
     }, 100);
 
     // 컴포넌트 언마운트 시 인터벌 정리
-    return () => clearInterval(id);
+    return () => {
+      clearInterval(id);
+      // 컴포넌트 언마운트 시 Unity 플레이어 종료
+      if (UnityModule) {
+        UnityModule.destroy();
+        console.log('UnityPlayer destroyed on unmount.');
+      }
+    };
   }, []);
 
-
+  useEffect(() => {
+    if (unityRef?.current) {
+      const message: IMessage = {
+        gameObject: 'gameObject',
+        methodName: 'methodName',
+        message: 'message',
+      };
+      unityRef.current.postMessage(
+        message.gameObject,
+        message.methodName,
+        message.message,
+      );
+    }
+  }, []);
 
   return (
     <View className="flex-1 bg-white px-4 justify-between">
@@ -26,11 +54,15 @@ export default function CapsuleUploadPage() {
         <Text className="text-2xl mb-5 text-center">업로드</Text>
 
         {/* AR 삽입 예정 */}
-        <Image
-          source={require('../assets/images/logo.png')}
-          className="w-[78px] h-[78px] self-center"
-        />
-
+        <View className="mt-8 w-11/12 h-4/5 self-center bg-grey2">
+          <UnityView
+            ref={unityRef}
+            style={{flex: 1}} // 부모 View의 크기를 채우도록 설정
+            onUnityMessage={result => {
+              console.log('onUnityMessage', result.nativeEvent.message);
+            }}
+          />
+        </View>
       </View>
       {/*
       <Progress.Bar // ▮ 막대(bar) 형태
